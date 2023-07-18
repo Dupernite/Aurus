d59fe3b25d775b365d32fe7c1d6451f59ab971fb
Dupernite <arthurtsmendes.ps4@gmail.com>
2023-07-18 17:31:10 +0100

    experience drop

--- src/main/java/com/dupernite/aurus/block/ModBlocks.java
+++ src/main/java/com/dupernite/aurus/block/ModBlocks.java
@@ -5,11 +5,13 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 
 public class ModBlocks {
@@ -33,8 +35,11 @@ public class ModBlocks {
 
     //Blocks
     public static final Block OVERWORLD_QUARTZ_ORE = registerBlock("overworld_quartz_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.STONE).strength(4.0f).requiresTool()));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE).strength(4.0f).requiresTool(),
+                    UniformIntProvider.create(2, 6)));
 
     public static final Block DEEPSLATE_OVERWORLD_QUARTZ_ORE = registerBlock("deepslate_overworld_quartz_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(6.0f).requiresTool()));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE).strength(6.0f).requiresTool(),
+                    UniformIntProvider.create(2, 6)));
+
 }
