4ac827a0d7ed4f3dfee8ce1fc2e962d1b1d4b41b
Dupernite <arthurtsmendes.ps4@gmail.com>
2023-08-05 22:09:55 +0100

    Refactored the `ModItem.java` to provide a more defined structure between food, ores, weapons, armors, and other items. Also introduced rarity classification for some items - crystalized soup and spear are now considered rare, netherite horse armor is classified as epic. The restructuring should provide clear categorization and the rarity classification underscores the importance of certain items.

--- src/main/java/com/dupernite/aurus/item/ModItem.java
+++ src/main/java/com/dupernite/aurus/item/ModItem.java
@@ -13,15 +13,26 @@ public class ModItem {
     public static void registerModItems()   {
         AurusMod.LOGGER.info("Registering Mod Items for" + AurusMod.MOD_ID);
     }
+
+    //Food
+    public static final Item CRYSTALIZED_SOUP = registerItem("crystalized_soup", new Item(new FabricItemSettings().rarity(Rarity.RARE).food(new FoodComponent.Builder().hunger(7).saturationModifier(0.5f).build())));
+
+    //Ore
     public static final Item QUARTZ_FRAGMENT = registerItem("quartz_fragment", new Item(new FabricItemSettings()));
-    public static final Item CRYSTALIZED_SOUP = registerItem("crystalized_soup", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(7).saturationModifier(0.5f).build())));
+
+    //Weapon
     public static final Item SPEAR = registerItem("spear", new ModSpearItem(new FabricItemSettings().maxCount(1).rarity(Rarity.RARE).maxDamage(10)));
-    public static final Item NETHERITE_HORSE_ARMOR = registerItem("netherite_horse_armor", new HorseArmorItem(14, "netherite", new FabricItemSettings()));
-    public static final Item COIN = registerItem("coin", new Item(new FabricItemSettings()));
+
+    //Armor
+    public static final Item NETHERITE_HORSE_ARMOR = registerItem("netherite_horse_armor", new HorseArmorItem(14, "netherite", new FabricItemSettings().rarity(Rarity.EPIC)));
     public static final Item DIVING_HELMET = registerItem("diving_helmet", new ArmorItem(ModArmorMaterial.DIVING, ArmorItem.Type.HELMET, new FabricItemSettings().rarity(Rarity.RARE)));
     public static final Item DIVING_CHESTPLATE = registerItem("diving_chestplate", new ArmorItem(ModArmorMaterial.DIVING, ArmorItem.Type.CHESTPLATE, new FabricItemSettings().rarity(Rarity.RARE)));
     public static final Item DIVING_LEGGINGS = registerItem("diving_leggings", new ArmorItem(ModArmorMaterial.DIVING, ArmorItem.Type.LEGGINGS, new FabricItemSettings().rarity(Rarity.RARE)));
     public static final Item DIVING_BOOTS = registerItem("diving_boots", new ArmorItem(ModArmorMaterial.DIVING, ArmorItem.Type.BOOTS, new FabricItemSettings().rarity(Rarity.RARE)));
+
+    //Others
+    public static final Item COIN = registerItem("coin", new Item(new FabricItemSettings()));
+
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(AurusMod.MOD_ID, name), item);
     }
