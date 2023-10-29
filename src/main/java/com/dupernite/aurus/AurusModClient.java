38cd2d1f27786aeb6fc94543ba3328e0853c2a05
Arthur <arthurtsmendes@gmail.com>
2023-10-29 18:06:52 +0000

    Improve game mod features and technical refactoring
    Increased the documentation tasks completion from 4% to 6%. Added custom blocks (i.e., PedestalBlock, PotBlock, TrophyBlock) in ModBlock An output slot was created, primarily used in GUI, preventing from putting items and only allows taking existing items. A PotBlock schema file, PedestalBlock schema, and a TrophyBlock were created for the custom blocks.

--- src/main/java/com/dupernite/aurus/AurusModClient.java
+++ src/main/java/com/dupernite/aurus/AurusModClient.java
@@ -1,14 +1,19 @@
 package com.dupernite.aurus;
 
+import com.dupernite.aurus.block.ModBlock;
 import com.dupernite.aurus.screen.ModScreenHandlers;
 import com.dupernite.aurus.screen.UpgraderScreen;
 import com.dupernite.aurus.util.ModModelPredicateProvider;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.RenderLayer;
 
 public class AurusModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlock.RELIC2, RenderLayer.getCutout());
+
         ModModelPredicateProvider.registerModModels();
 
         HandledScreens.register(ModScreenHandlers.UPGRADER_SCREEN_HANDLER, UpgraderScreen::new);
