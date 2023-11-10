99f826eafd7a84a92595f8ebc7ac0cd1c227e4f4
Arthur <arthurtsmendes@gmail.com>
2023-11-10 20:14:17 +0000

    Added Waterlogging to PotBLock, TrophyBlock and PedestalBlock

--- src/main/java/com/dupernite/aurus/block/custom/PedestalBlock.java
+++ src/main/java/com/dupernite/aurus/block/custom/PedestalBlock.java
@@ -1,25 +1,34 @@
 package com.dupernite.aurus.block.custom;
 
 import net.minecraft.block.*;
+import net.minecraft.entity.ai.pathing.NavigationType;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
 import java.util.stream.Stream;
 
-public class PedestalBlock extends HorizontalFacingBlock {
+public class PedestalBlock extends HorizontalFacingBlock implements Waterloggable{
     public static final DirectionProperty FACING;
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public PedestalBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(WATERLOGGED, false));
     }
 
     private static final VoxelShape SHAPE = Stream.of(
@@ -37,7 +46,11 @@ public class PedestalBlock extends HorizontalFacingBlock {
     }
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+
+        return this.getDefaultState()
+                .with(FACING, ctx.getHorizontalPlayerFacing().getOpposite())
+                .with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER || fluidState.getFluid() == Fluids.FLOWING_WATER);
     }
 
     @Override
@@ -58,10 +71,52 @@ public class PedestalBlock extends HorizontalFacingBlock {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, WATERLOGGED);
     }
 
     static {
         FACING = Properties.HORIZONTAL_FACING;
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {
+        return false;
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public boolean tryFillWithFluid(WorldAccess world, BlockPos pos, BlockState state, FluidState fluidState) {
+        if (!state.get(WATERLOGGED) && fluidState.getFluid() == Fluids.WATER) {
+            if (!world.isClient()) {
+                world.setBlockState(pos, state.with(WATERLOGGED, true), 3);
+                world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            }
+            return true;
+        } else {
+            return Waterloggable.super.tryFillWithFluid(world, pos, state, fluidState);
+        }
+    }
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        if (state.get(WATERLOGGED)) {
+            return super.canPlaceAt(state, world, pos);
+        } else {
+            return super.canPlaceAt(state, world, pos) && this.canPlaceBlockOn(world.getBlockState(pos.down()), world, pos.down());
+        }
+    }
+
+    private boolean canPlaceBlockOn(BlockState floor, BlockView world, BlockPos pos) {
+        return floor.isSideSolidFullSquare(world, pos, Direction.UP);
     }
 }
